838cc8fcb619f46bebe9ed506985460b5d3e8e8e
oded-saban <odeds@activefence.com>
2022-07-19 09:03:39 +0300

    add routes

--- server/src/mongo.js
+++ server/src/mongo.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
-
+const collection = 'processes';
 class Mongo {
     constructor(connectionString) {
         this.mongo = mongoose.createConnection(connectionString);
     }
 
     async insert(data) {
-        return await this.mongo.collection('processes').insertOne(data);
+        return await this.mongo.collection(collection).insertOne(data);
     }
 
     async find(data) {
-        return await this.mongo.collection('processes').findOne(data);
+        return await this.mongo.collection(collection).findOne(data);
+    }
+
+    async findAll() {
+        return this.mongo.collection(collection).find({});
     }
 
     async update(filter, data) {
-        await this.mongo.collection('processes').updateOne(filter, { $set: data }, { upsert: true });
+        await this.mongo.collection(collection).updateOne(filter, { $set: data }, { upsert: true });
     }
 
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -6,21 +6,39 @@ const connectionString = 'mongodb+srv://username:username@cluster.a1jyk.mongodb.
 const mongo = new Mongo(connectionString);
 
 router.get('/process', async (req, res) => {
-    const {id: processId} = req.body;
-    return await mongo.find({processId})
+    try {
+        const {id: processId} = req.body;
+        const processData = await mongo.find({processId});
+        return res.status(200).send(processData);
+    } catch (e) {
+        console.log('Error ==>', e);
+        return res.status(500);
+    }
 });
 
 router.get('/all-processes', async (req, res) =>{
+    try {
+        const processesData = mongo.findAll();
+        return res.status(200).send(processesData);
+    } catch (e) {
+        console.log('Error ==>', e);
+        return res.status(500);
 
+    }
 });
 
 router.post('/process', async (req, res) =>{
-
+    try {
+        const data = req.body;
+        return await mongo.insert(data);
+    } catch (e) {
+        console.log('Error ==>', e);
+        return res.status(500);
+    }
 });
 
+router.post('/store-process', async (req, res) => {
 
-router.post('/', async (req, res) =>{
-
-});
+})
 
 module.exports = router;
